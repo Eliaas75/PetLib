bc8f4956eac915d9f3cf52edda443e580065a8b8
Diamant <93088364+Eliaas75@users.noreply.github.com>
2026-09-17 13:54:24 +0100

    feat(pro): mount availability management routes

--- server/src/index.js
+++ server/src/index.js
@@ -11,6 +11,7 @@ import availabilityRoutes from "./routes/availability.js";
 import searchRoutes from "./routes/search.js";
 import appointmentRoutes from "./routes/appointments.js";
 import waitlistRoutes from "./routes/waitlist.js";
+import proAvailabilityRoutes from "./routes/proAvailability.js";
 import proRoutes from "./routes/pro.js";
 import { startWaitlistSweeper } from "./services/waitlist.js";
 
@@ -46,6 +47,7 @@ app.use("/api/availability", availabilityRoutes);
 app.use("/api/search", searchRoutes);
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/waitlist", waitlistRoutes);
+app.use("/api/pro/availability", proAvailabilityRoutes);
 app.use("/api/pro", proRoutes);
 
 app.use((_req, res) => {
